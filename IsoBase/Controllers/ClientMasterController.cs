131f71b8ea4fe8e88cff6bcde010b7529b442c19
Dariaman Sigaian <dariaman.siagian@gmail.com>
2019-07-16 14:32:07 +0700

    paging done
    belum termasuk search and order

--- IsoBase/Controllers/ClientMasterController.cs
+++ IsoBase/Controllers/ClientMasterController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using IsoBase.Data;
 using IsoBase.Models;
+using DataTables.AspNetCore.Mvc.Binder;
 
 namespace IsoBase.Controllers
 {
@@ -22,7 +23,37 @@ namespace IsoBase.Controllers
         // GET: ClientMaster
         public async Task<IActionResult> Index()
         {
-            return View(await _context.ClientMasterModels.ToListAsync());
+            return View(await _context.ClientMasterModel.ToListAsync());
+        }
+
+        public IActionResult Index2()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        //[ValidateAntiForgeryToken]
+        public IActionResult PageData([DataTablesRequest] DataTablesRequest dataRequest)
+        {
+            IEnumerable<ClientMasterModel> products = _context.ClientMasterModel.Skip(dataRequest.Start).Take(dataRequest.Length);
+            int recordsTotal = 2000; // _context.Tabel1Model.Count();
+            //int recordsTotal = products.Count();
+            int recordsFilterd = 1000; // recordsTotal;
+
+            return Json(products
+                .Select(e => new
+                {
+                    e.ClientID,
+                    e.ClientCode,
+                    e.Name,
+                    e.ClientTypeID,
+                    e.UserCreate,
+                    e.DateCreate,
+                    e.UserUpdate,
+                    e.DateUpdate
+                })
+                .ToDataTablesResponse(dataRequest, recordsTotal, recordsFilterd));
         }
 
         // GET: ClientMaster/Details/5
@@ -33,7 +64,7 @@ namespace IsoBase.Controllers
                 return NotFound();
             }
 
-            var clientMasterModels = await _context.ClientMasterModels
+            var clientMasterModels = await _context.ClientMasterModel
                 .FirstOrDefaultAsync(m => m.ClientID == id);
             if (clientMasterModels == null)
             {
@@ -54,7 +85,7 @@ namespace IsoBase.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("ClientID,ClientCode,Name,ClientTypeID,IsActive,UserCreate,DateCreate,UserUpdate,DateUpdate")] ClientMasterModels clientMasterModels)
+        public async Task<IActionResult> Create([Bind("ClientID,ClientCode,Name,ClientTypeID,IsActive,UserCreate,DateCreate,UserUpdate,DateUpdate")] ClientMasterModel clientMasterModels)
         {
             if (ModelState.IsValid)
             {
@@ -73,7 +104,7 @@ namespace IsoBase.Controllers
                 return NotFound();
             }
 
-            var clientMasterModels = await _context.ClientMasterModels.FindAsync(id);
+            var clientMasterModels = await _context.ClientMasterModel.FindAsync(id);
             if (clientMasterModels == null)
             {
                 return NotFound();
@@ -86,7 +117,7 @@ namespace IsoBase.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ClientID,ClientCode,Name,ClientTypeID,IsActive,UserCreate,DateCreate,UserUpdate,DateUpdate")] ClientMasterModels clientMasterModels)
+        public async Task<IActionResult> Edit(int id, [Bind("ClientID,ClientCode,Name,ClientTypeID,IsActive,UserCreate,DateCreate,UserUpdate,DateUpdate")] ClientMasterModel clientMasterModels)
         {
             if (id != clientMasterModels.ClientID)
             {
@@ -147,7 +178,7 @@ namespace IsoBase.Controllers
 
         private bool ClientMasterModelsExists(int id)
         {
-            return _context.ClientMasterModels.Any(e => e.ClientID == id);
+            return _context.ClientMasterModel.Any(e => e.ClientID == id);
         }
     }
 }
--- IsoBase/Startup.cs
+++ IsoBase/Startup.cs
@@ -44,6 +44,7 @@ namespace IsoBase
                 .AddEntityFrameworkStores<UserDbContext>();
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            //services.AddMvc(options =>{ options.Filters.Add(new AutoValidateAntiforgeryTokenAttribute()); });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
